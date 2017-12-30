163b72ce9cf0e161dc2a2f6750029905a0c842da
Yuanyuan Zhou <zhou.yua@husky.neu.edu>
2017-12-29 21:46:31 -0500

    JavaScript

---
+++ benefits.js
@@ -0,0 +1,11 @@
+function displayAppleBenfits(){
+	
+	document.getElementById("appleframe").style.display = "block";
+	
+}
+
+function displayOrangeBenfits(){
+	
+	document.getElementById("orangeframe").style.display = "block"
+	
+}
